47a47cb4937ddcec2c3daf6b48edbb34ed57118f
Phạm Quang Trường <truongpham090325@gmail.com>
2026-05-25 16:09:03 +0700

    Update header

--- controllers/client/home.controller.ts
+++ controllers/client/home.controller.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from "express";
 import Product from "../../models/product.model";
+import CategoryProduct from "../../models/category-product.model";
 
 export const home = async (req: Request, res: Response) => {
   try {
+    const categoryProductList = await CategoryProduct.find({
+      deleted: false,
+    });
+
     res.render("client/pages/home", {
       pageTitle: "Trang chủ",
+      categoryProductList: categoryProductList,
     });
   } catch (error) {
     console.log(error);
-    res.redirect("/");
   }
 };
