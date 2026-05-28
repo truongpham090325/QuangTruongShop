9cfa1c5b8c1ffe56c14f6355c02161c6269c7f0e
Phạm Quang Trường <truongpham090325@gmail.com>
2026-05-28 16:07:50 +0700

    Bổ sung phần phân quyền

--- configs/variable.config.ts
+++ configs/variable.config.ts
@@ -8,6 +8,66 @@ export const permissionList = [
     name: "Trang tổng quan",
   },
   {
+    id: "product-category-list",
+    name: "Danh sách danh mục sản phẩm",
+  },
+  {
+    id: "prodcut-category-create",
+    name: "Tạo danh mục sản phẩm",
+  },
+  {
+    id: "product-category-edit",
+    name: "Sửa danh mục sản phẩm",
+  },
+  {
+    id: "product-category-delete",
+    name: "Xóa danh mục sản phẩm",
+  },
+  {
+    id: "product-category-trash",
+    name: "Thùng rác danh mục sản phẩm",
+  },
+  {
+    id: "view-price-old",
+    name: "Xem giá nhập",
+  },
+  {
+    id: "product-list",
+    name: "Danh sách sản phẩm",
+  },
+  {
+    id: "prodcut-create",
+    name: "Tạo sản phẩm",
+  },
+  {
+    id: "product-edit",
+    name: "Sửa sản phẩm",
+  },
+  {
+    id: "product-delete",
+    name: "Xóa sản phẩm",
+  },
+  {
+    id: "product-trash",
+    name: "Thùng rác sản phẩm",
+  },
+  {
+    id: "order-list",
+    name: "Danh sách đơn hàng",
+  },
+  {
+    id: "order-edit",
+    name: "Sửa đơn hàng",
+  },
+  {
+    id: "order-delete",
+    name: "Xóa đơn hàng",
+  },
+  {
+    id: "order-trash",
+    name: "Thùng rác đơn hàng",
+  },
+  {
     id: "role-list",
     name: "Danh sách nhóm quyền",
   },
@@ -21,15 +81,39 @@ export const permissionList = [
   },
   {
     id: "role-delete",
-    name: "Xóa nhóm quyền",
+    name: "Xóa nhóm quyến",
   },
   {
     id: "role-trash",
     name: "Thùng rác nhóm quyền",
   },
   {
-    id: "view-price-old",
-    name: "Xem giá nhập",
+    id: "account-admin-list",
+    name: "Danh sách tài khoản quản trị",
+  },
+  {
+    id: "account-admin-create",
+    name: "Tạo tài khoản quản trị",
+  },
+  {
+    id: "account-admin-edit",
+    name: "Sửa tài khoản quản trị",
+  },
+  {
+    id: "account-admin-delete",
+    name: "Xóa tài khoản quản trị",
+  },
+  {
+    id: "account-admin-trash",
+    name: "Thùng rác tài khoản quản trị",
+  },
+  {
+    id: "account-admin-change-password",
+    name: "Đổi mật khẩu tài khoản quản trị",
+  },
+  {
+    id: "admin-log",
+    name: "Lịch sử hoạt động",
   },
   {
     id: "file-manager",
